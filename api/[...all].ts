6de422b9a4cefdc80d63b93684c031da87d12064
Shreyas <shreyasmshanbhag@gmail.com>
2025-11-11 23:35:00 +0530

    build(vercel): avoid TS7016 by requiring compiled app.js; ensure dist included

--- api/[...all].ts
+++ api/[...all].ts
@@ -1,3 +1,5 @@
-import app from '../apps/api/dist/src/app.js';
+// Use CommonJS require to avoid TS7016 for untyped compiled JS during build
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const app = require('../apps/api/dist/src/app.js');
 
 export default app as any;
