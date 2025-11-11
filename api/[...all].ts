7c486ad5f131638471ca2d2f09191fe2acba4b8e
Shreyas <shreyasmshanbhag@gmail.com>
2025-11-11 23:39:13 +0530

    fix(api-proxy): export compiled app default or module for Vercel runtime interop

--- api/[...all].ts
+++ api/[...all].ts
@@ -1,5 +1,7 @@
 // Use CommonJS require to avoid TS7016 for untyped compiled JS during build
+// Ensure compatibility with both CJS and ESM default exports
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const app = require('../apps/api/dist/src/app.js');
+const appModule = require('../apps/api/dist/src/app.js');
+const app = appModule?.default || appModule;
 
 export default app as any;
